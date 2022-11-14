4c5602b21ff9ca7bc2934aba9ad41ac5d39e611b
ranjabi <fikriranjabi02@gmail.com>
2022-11-14 21:14:28 +0700

    feat: step 2 three way handshake

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import lib.connection
 from lib.segment import Segment
 import lib.segment as segment
+import random
 
 class Client:
     def __init__(self, host, port):
@@ -9,16 +10,29 @@ class Client:
         self.port = port
         self.conn = lib.connection.Connection(host,port)
         self.segment = segment.Segment()
-        self.segment.set_payload(b'test send data')
-        self.segment.set_flag([1, 0, 0])
-
-        print("Sending data...")
-        print(self.segment)
-        self.conn.send_data(self.segment, (host, port))
+        print(f" [!] Client started at {self.host}:{self.port}"))
 
     def three_way_handshake(self):
         # Three Way Handshake, client-side
-        pass
+        # 1 SYN, initiate connection
+        self.segment.set_flag([0, 1, 0]) # SYN flag
+        seqNum = random.randint(0,2**32-1)
+        self.segment.set_payload(b'test send data')
+        self.segment.set_header({
+        'seq_num': seqNum,
+        'ack_num': 0,
+        'flag': self.segment.get_flag(),
+        'payload': self.segment.get_payload()
+        })
+        print("[!] Initiating three way handshake...")
+
+        self.conn.send_data(self.segment, (self.host,self.port))
+        print(f"[!] [Handshake] Sending broadcast SYN request to port {self.port}")
+        print("[!] [Handshake] Waiting for response...")
+
+        data, addr = self.conn.listen_single_segment()
+        print("[!] [Handshake] SYN-ACK received.", data)
+        print("[!] [Handshake] Sending ACK")
 
     def listen_file_transfer(self):
         # File transfer, client-side
@@ -27,5 +41,5 @@ class Client:
 
 if __name__ == '__main__':
     main = Client('localhost',50007)
-    # main.three_way_handshake()
+    main.three_way_handshake()
     # main.listen_file_transfer()
--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from typing import Tuple
+from typing import Union
 import lib.connection
 from lib.segment import Segment
 import lib.segment as segment
 import socket
-import time
+import random
 
 class Server:
     def __init__(self, host, port):
@@ -12,6 +12,7 @@ class Server:
         self.port = port
         self.conn = lib.connection.Connection(self.host,self.port)
         self.conn.socket.bind((self.host,self.port))
+        self.clients = {}
         print(f"[!] Server started at {self.host}:{self.port}")
         print(f"[!] Source file | unset | x bytes")
         print("[!] Listening to broadcast address for clients.")
@@ -19,35 +20,54 @@ class Server:
     def listen_for_clients(self):
         # Server listening
         listening = True
-        clientList = []
         while listening:
-            try:
-                receivedSeg, addr = self.conn.listen_single_segment()
+                data, addr = self.conn.listen_single_segment()
                 print(f"[!] Received request from {addr[0]}:{addr[1]}")
-                clientList.append(addr)
+                self.clients[addr] = data
                 isListenMore = input("Listen more? (y/n)")
                 if isListenMore == "n":
+                    # print(data)
                     break
-            except socket.error as e:
-                break
         print("\nClient list:")
-        for idx, val in enumerate(clientList):
-            print(f"{idx+1}. {val[0]}:{val[1]}")
+        for idx, (key, _val) in enumerate(self.clients.items()):
+            print(f"{idx+1}. {key[0]}:{key[1]}")
+        print("")
 
     def start_file_transfer(self):
         # Handshake & file transfer for all client
-        pass
+        for idx, (key, val) in enumerate(self.clients.items()):
+            host = key[0]
+            port = key[1]
+            print("[!] Commencing file transfer...")
+            print(f"[!] [Handshake] Handshake to client {idx+1}...")
+            self.three_way_handshake((host, port))
 
-    def file_transfer(self, client_addr : Tuple["ip", "port"]):
+    def file_transfer(self, client_addr : Union[str, int]):
         # File transfer, server-side, Send file to 1 client
         pass
 
-    def three_way_handshake(self, client_addr: Tuple["ip", "port"]) -> bool:
-       # Three way handshake, server-side, 1 client
-       pass
+    def three_way_handshake(self, client_addr: Union[str, int]) -> bool:
+        # Three way handshake, server-side, 1 client
+
+        # STEP 2: SYN-ACK from server
+        print(f"[!] [Handshake] Sending SYN-ACK")
+        data: Segment = self.clients[client_addr]
+        header = data.get_header()
+        clientACK = header['seq_num']
+        seqNum = random.randint(0,2**32-1)
+        data.set_header({
+        'seq_num': seqNum,
+        'ack_num': clientACK+1,
+    })
+        data.set_flag([0,1,1])
+        self.conn.send_data(data, client_addr)
+        while True:
+            data, addr = self.conn.listen_single_segment()
+            if (data):
+                print("received")
 
 
 if __name__ == '__main__':
     main = Server('localhost',50007)
     main.listen_for_clients()
-    # main.start_file_transfer()
+    main.start_file_transfer()
