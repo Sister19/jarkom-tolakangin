f0d4322b21ab55e3c1b5fb5db38bffd16dbbd0b3
ranjabi <fikriranjabi02@gmail.com>
2022-11-14 18:01:41 +0700

    fix: listen to many client

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import lib.connection
 from lib.segment import Segment
 import lib.segment as segment
 import socket
+import time
 
 class Server:
     def __init__(self, host, port):
@@ -12,12 +13,26 @@ class Server:
         self.conn = lib.connection.Connection(self.host,self.port)
         self.conn.socket.bind((self.host,self.port))
         print(f"[!] Server started at {self.host}:{self.port}")
+        print(f"[!] Source file | unset | x bytes")
+        print("[!] Listening to broadcast address for clients.")
 
     def listen_for_clients(self):
         # Server listening
         listening = True
+        clientList = []
         while listening:
-            print(self.conn.listen_single_segment())
+            try:
+                receivedSeg, addr = self.conn.listen_single_segment()
+                print(f"[!] Received request from {addr[0]}:{addr[1]}")
+                clientList.append(addr)
+                isListenMore = input("Listen more? (y/n)")
+                if isListenMore == "n":
+                    break
+            except socket.error as e:
+                break
+        print("\nClient list:")
+        for idx, val in enumerate(clientList):
+            print(f"{idx+1}. {val[0]}:{val[1]}")
 
     def start_file_transfer(self):
         # Handshake & file transfer for all client
