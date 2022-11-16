cf155985259a2d8f1c968230c75a330fea21a659
ranjabi <fikriranjabi02@gmail.com>
2022-11-17 00:45:26 +0700

    fix: tcp sequence multiple segment

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import lib.connection
 from lib.segment import Segment
 import lib.segment as segment
 import argparse
+import os
 
 class Client:
     def __init__(self, host, port, destPort, outputPath):
@@ -13,30 +14,27 @@ class Client:
         self.segment = segment.Segment()
         self.outputPath = outputPath
         self.payload = None
-        print(f" [!] Client started at {self.host}:{self.port}")
+        print(f"[!] Client started at {self.host}:{self.port}")
 
     def three_way_handshake(self):
         # Three Way Handshake, client-side
+
         # STEP 1: SYN, initiate connection
+        print("[!] Initiating three way handshake...")
         self.segment.set_flag([0, 1, 0]) # SYN flag
         seqNum = 0
         self.segment.set_header({
         'seq_num': seqNum,
-        'ack_num': 0,
-        'flag': self.segment.get_flag(),
-        'payload': self.segment.get_payload()
+        'ack_num': 0
         })
-
-        print("[!] Initiating three way handshake...")
-
         self.conn.send_data(self.segment, (self.host,self.destPort))
-
         print(f"[!] [Handshake] Sending broadcast SYN request to port {self.destPort}")
+
         # print(self.segment)
         print("[!] [Handshake] Waiting for response...")
 
         data, addr = self.conn.listen_single_segment()
-        if data.get_syn() == 1 and data.get_ack() == 1:
+        if data.get_syn() and data.get_ack():
             if data.valid_checksum():
                 print("[!] [Handshake] SYN-ACK received.")
 
@@ -49,29 +47,32 @@ class Client:
                 'seq_num': serverACK,
                 'ack_num': serverSeq+1,
                 })
-                print("[!] [Handshake] Connection established. Sending ACK.")
                 self.conn.send_data(data, (self.host,self.destPort))
+                print("[!] [Handshake] Connection established. Sending ACK.")
                 # print(data)
             else:
                 print("[!] [Handhshake] Checksum failed. Connection is terminated.")
 
     def listen_file_transfer(self):
         # File transfer, client-side
-        data, addr = self.conn.listen_single_segment()
         
-        if not(data.get_syn()) and not(data.get_ack()) and not(data.get_fin()):
-            header = data.get_header()
-            serverACK = header['ack_num']
-            serverSeq = header['seq_num']
-            data.set_header({
-            'seq_num': serverACK,
-            'ack_num': serverSeq+1,
-            })
-            data.set_flag([0,0,1])
-            print(f"[Segment SEQ={serverSeq}] Received, Ack sent")
-            self.conn.send_data(data, (self.host,self.destPort))
-        with open(self.outputPath, 'wb') as f:
-            f.write(data.get_payload())
+        # TODO: IMPLEMENT CLOSING CONNECTION
+        for i in range(6):
+            data, addr = self.conn.listen_single_segment()
+            if not(data.get_syn()) and not(data.get_ack()) and not(data.get_fin()):
+                # print('payload',data.get_payload())
+                header = data.get_header()
+                data.set_header({
+                'seq_num': header['ack_num'],
+                'ack_num': header['seq_num']+len(data.get_payload()),
+                })
+                data.set_flag([0,0,1])
+                self.conn.send_data(data, (self.host,self.destPort))
+                
+                print(f"[Segment SEQ={header['seq_num']}] Received, Ack sent")
+                with open(self.outputPath, 'ab') as f:
+                    f.write(data.get_payload())
+                    f.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -79,6 +80,12 @@ if __name__ == '__main__':
     parser.add_argument("broadcastPort", type=int)
     parser.add_argument("outputPath", type=str)
     args = parser.parse_args()
+
+    # Delete if previous file exist
+    try:
+        os.remove(args.outputPath)
+    except OSError:
+        pass
 
     main = Client('localhost', args.clientPort, args.broadcastPort, args.outputPath)
     
--- server.py
+++ server.py
@@ -16,6 +16,7 @@ class Server:
         self.filepath = filepath
         self.payload = self.read_binary()
         self.filesize = os.path.getsize(self.filepath)
+        self.readOffset = 0
         print(f"[!] Server started at {self.host}:{self.port}")
         print(f"[!] Source file | {self.filepath} | {self.filesize} bytes")
         print("[!] Listening to broadcast address for clients.")
@@ -52,57 +53,87 @@ class Server:
             print(f"[!] [Handshake] Handshake to client {idx+1}...")
             self.three_way_handshake((host, port))
 
-    def file_transfer(self, client_addr : Union[str, int]):
-        # File transfer, server-side, Send file to 1 client
-        data: Segment = self.clients[client_addr]
-        header = data.get_header()
-        prevSeq = header['seq_num']
-        prevACK = header['ack_num']
-        data.set_flag([0,0,0])
-        data.set_header({
-            'seq_num': prevACK,
-            'ack_num': prevSeq+1
-        })
-        data.set_payload(self.payload)
-        self.conn.send_data(data, client_addr)
-        print(f"[Segment SEQ={prevACK}] Sent")
-
-        resData, addr = self.conn.listen_single_segment()
-        header = resData.get_header()
-        prevSeq = header['seq_num']
-        prevACK = header['ack_num']
-        if resData.get_ack():
-            print(f"[Segment SEQ={prevACK-1}] Acked")
-
     def three_way_handshake(self, client_addr: Union[str, int]) -> bool:
         # Three way handshake, server-side, 1 client
 
         # STEP 2: SYN-ACK from server to client
         data: Segment = self.clients[client_addr]
-        if data.get_syn() == 1:
+        if data.get_syn():
             if data.valid_checksum():
-                print(f"[!] [Handshake] Sending SYN-ACK")
                 header = data.get_header()
                 clientACK = header['seq_num']
-                seqNum = 300
+                data.set_flag([0,1,1])
                 data.set_header({
-                'seq_num': seqNum,
+                'seq_num': 0,
                 'ack_num': clientACK+1,
                 })
-                data.set_flag([0,1,1])
                 # print(data)
                 self.conn.send_data(data, client_addr)
+                print(f"[!] [Handshake] Sending SYN-ACK")
 
-                dataEstablished, addr = self.conn.listen_single_segment()
-                if dataEstablished.get_ack() == 1:
-                    if dataEstablished.valid_checksum():
-                        print("[!] [Handshake] Connection established.")
+                data, addr = self.conn.listen_single_segment()
+                if data.get_ack():
+                    if data.valid_checksum():
+                        print("[!] [Handshake] Connection established.\n")
+                        self.clients[client_addr] = data
                         self.file_transfer(client_addr)
                     else:
                         print("[!] [Handhshake] Checksum failed. Connection is terminated.")
             else:
                 print("[!] [Handhshake] Checksum failed. Connection is terminated.")
 
+    def file_transfer(self, client_addr : Union[str, int]):
+        size = 32756
+        while (self.filesize > size):
+            self.filesize -= size
+            
+            with open(self.filepath, 'rb') as f:
+                data = self.clients[client_addr]
+                header = data.get_header()
+                data.set_header({
+                    'seq_num': header['ack_num'],
+                    'ack_num': header['seq_num']
+                })
+                temp = header['ack_num']
+                data.set_flag([0,0,0])
+                f.seek(self.readOffset)
+                payload = f.read(size)
+                # print('payload',payload)
+                data.set_payload(payload)
+                self.readOffset += size
+                f.close()
+
+                self.conn.send_data(data, client_addr)
+                print(f"[Segment SEQ={header['ack_num']} Sent")
+
+                resData, addr = self.conn.listen_single_segment()
+                if resData.get_ack():
+                    self.clients[client_addr] = resData
+                    header = resData.get_header()
+                    print(f"[Segment SEQ={temp}] Acked")
+        else:
+            print('masuk else, sisa',self.filesize)
+            with open(self.filepath, 'rb') as f:
+                data = self.clients[client_addr]
+                header = data.get_header()
+                data.set_header({
+                    'seq_num': header['ack_num'],
+                    'ack_num': header['seq_num']
+                })
+                temp = header['ack_num']
+                data.set_flag([0,0,0])
+                f.seek(self.readOffset)
+                data.set_payload(f.read(self.filesize))
+                f.close()
+
+                self.conn.send_data(data, client_addr)
+                print(f"[Segment SEQ={header['ack_num']} Sent")
+
+                resData, addr = self.conn.listen_single_segment()
+                if resData.get_ack():
+                    self.clients[client_addr] = resData
+                    header = resData.get_header()
+                    print(f"[Segment SEQ={temp}] Acked")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
