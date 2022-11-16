8a54de94e3a71ec05f7ad92a4cf87d253f389952
dikyrest <16520040@std.stei.itb.ac.id>
2022-11-17 03:28:08 +0700

    feat: go back n protocol for server

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from typing import Union
 import lib.connection
-from lib.segment import Segment
-import lib.segment as segment
+import lib.constant
+import lib.segment
+import math
 import argparse
 import os
 
@@ -14,19 +15,12 @@ class Server:
         self.conn.socket.bind((self.host,self.port))
         self.clients = {}
         self.filepath = filepath
-        self.payload = self.read_binary()
         self.filesize = os.path.getsize(self.filepath)
+        self.windowSize = lib.constant.WINDOW_SIZE
         print(f"[!] Server started at {self.host}:{self.port}")
         print(f"[!] Source file | {self.filepath} | {self.filesize} bytes")
         print("[!] Listening to broadcast address for clients.")
         # print(self.payload)
-
-    def read_binary(self):
-        binary = None
-        with open(self.filepath, 'rb') as f:
-            binary = f.read()
-            f.close()
-        return binary
 
     def listen_for_clients(self):
         # Server listening
@@ -54,31 +48,112 @@ class Server:
 
     def file_transfer(self, client_addr : Union[str, int]):
         # File transfer, server-side, Send file to 1 client
-        data: Segment = self.clients[client_addr]
-        header = data.get_header()
-        prevSeq = header['seq_num']
-        prevACK = header['ack_num']
-        data.set_flag([0,0,0])
-        data.set_header({
-            'seq_num': prevACK,
-            'ack_num': prevSeq+1
-        })
-        data.set_payload(self.payload)
-        self.conn.send_data(data, client_addr)
-        print(f"[Segment SEQ={prevACK}] Sent")
+        file = open(self.filepath, "rb")
+        windowSize = self.windowSize
+        sequenceBase = 0
+        sequenceMax = int(windowSize) + 1
+        maxSegment = math.ceil(self.filesize / lib.constant.MAX_DATA_SIZE)
+        goBackN = False
+        print(self.filesize)
 
-        resData, addr = self.conn.listen_single_segment()
-        header = resData.get_header()
-        prevSeq = header['seq_num']
-        prevACK = header['ack_num']
-        if resData.get_ack():
-            print(f"[Segment SEQ={prevACK-1}] Acked")
+        # Loop until all segments are sent
+        while sequenceBase < maxSegment:
+            # Loop until window is full
+            sequenceNum = int(sequenceBase) + 1
+            while sequenceNum < min(maxSegment+1, sequenceMax):
+                if (sequenceNum != maxSegment):
+                    data: lib.segment.Segment = self.clients[client_addr]
+                    data.set_flag([0,0,0])
+                    file.seek((sequenceNum-1)*lib.constant.MAX_DATA_SIZE)
+                    data.set_payload(file.read(lib.constant.MAX_DATA_SIZE))
+                    data.set_header({
+                        "seq_num": sequenceNum-1,
+                    })
+                    self.conn.send_data(data, client_addr)
+                    print(f"[Segment SEQ={sequenceNum}] Sent to {client_addr[0]}:{client_addr[1]}")
+                    sequenceNum += 1
+                else:
+                    data: lib.segment.Segment = self.clients[client_addr]
+                    data.set_flag([1,0,0])
+                    file.seek((sequenceNum-1)*lib.constant.MAX_DATA_SIZE)
+                    data.set_payload(file.read(lib.constant.MAX_DATA_SIZE))
+                    data.set_header({
+                        "seq_num": sequenceNum-1,
+                    })
+                    self.conn.send_data(data, client_addr)
+                    print(f"[Segment SEQ={sequenceNum}] Sent to {client_addr[0]}:{client_addr[1]}")
+                    sequenceNum += 1
+            
+            # Wait for ACK
+            for i in range(int(sequenceBase)+1, int(sequenceNum)):
+                rcvSeg, addr = self.conn.listen_single_segment()
+                requestNum = int(rcvSeg.get_header()['ack_num'])
+
+                # If ACK is valid
+                if (requestNum > sequenceBase and addr[1] == client_addr[1]):
+                    sequenceMax = int(sequenceMax) + int(sequenceBase) + int(requestNum)
+                    sequenceBase = int(requestNum)
+                    print(f"[Segment SEQ={i}] Acked")
+                # If ACK is invalid
+                elif (addr[1] != client_addr[1]):
+                    goBackN = True
+                    print(f"[Segment SEQ={i}] Not Acked. Wrong client.")
+                    print(f"Commencing Go-Back-N protocol from Segment SEQ={i} to {client_addr[0]}:{client_addr[1]}")
+                    break
+                else:
+                    goBackN = True
+                    print(f"[Segment SEQ={i}] Not Acked. Duplicate ACK found.")
+                    print(f"Commencing Go-Back-N protocol from Segment SEQ={i} to {client_addr[0]}:{client_addr[1]}")
+                    break
+        
+        if goBackN:
+            print("[!] Go-Back-N protocol success")
+        print("[!] File transfer completed")
+        file.close()
+        self.close_connection(client_addr)
+
+        # data: Segment = self.clients[client_addr]
+        # header = data.get_header()
+        # prevSeq = header['seq_num']
+        # prevACK = header['ack_num']
+        # data.set_flag([0,0,0])
+        # data.set_header({
+        #     'seq_num': prevACK,
+        #     'ack_num': prevSeq+1
+        # })
+        # data.set_payload(self.payload)
+        # self.conn.send_data(data, client_addr)
+        # print(f"[Segment SEQ={prevACK}] Sent")
+
+        # resData, addr = self.conn.listen_single_segment()
+        # header = resData.get_header()
+        # prevSeq = header['seq_num']
+        # prevACK = header['ack_num']
+        # if resData.get_ack():
+        #     print(f"[Segment SEQ={prevACK-1}] Acked")
+
+    def close_connection(self, client_addr : Union[str, int]):
+        # Close connection, server-side
+        print("[!] Closing connection...")
+
+        print("Sending ACK from server...")
+        tw = lib.segment.Segment()
+        tw.set_flag([0,0,1])
+        self.conn.send_data(tw, client_addr)
+        
+        print("Sending FIN from server...")
+        tw2 = lib.segment.Segment()
+        tw2.set_flag([1,0,0])
+        self.conn.send_data(tw2, client_addr)
+
+        print("Get ACK close from client")
+        print(f"[!] Connection closed with {client_addr[0]}:{client_addr[1]}")
 
     def three_way_handshake(self, client_addr: Union[str, int]) -> bool:
         # Three way handshake, server-side, 1 client
 
         # STEP 2: SYN-ACK from server to client
-        data: Segment = self.clients[client_addr]
+        data: lib.segment.Segment = self.clients[client_addr]
         if data.get_syn() == 1:
             if data.valid_checksum():
                 print(f"[!] [Handshake] Sending SYN-ACK")
