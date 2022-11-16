acb347bbdd09c1c489fc2a5826b4010ed792d112
dikyrest <16520040@std.stei.itb.ac.id>
2022-11-17 03:28:24 +0700

    feat: go back n protocol for client

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 import lib.connection
-from lib.segment import Segment
 import lib.segment as segment
 import argparse
 
@@ -57,21 +56,87 @@ class Client:
 
     def listen_file_transfer(self):
         # File transfer, client-side
-        data, addr = self.conn.listen_single_segment()
+        requestNum = 0
+        file = open(self.outputPath, "ab", newline=None)
+        error = False
+
+        while True:
+            rcvSeg, addr = self.conn.listen_single_segment()
+            sequenceNum = int(rcvSeg.get_header()['seq_num'])
+            if (sequenceNum == requestNum):
+                requestNum = sequenceNum + 1
+                if (not rcvSeg.get_fin()):
+                    file.write(rcvSeg.get_payload())
+                    seg = segment.Segment()
+                    seg.set_flag([0,0,1])
+                    seg.set_header({
+                        'seq_num': sequenceNum,
+                        'ack_num': requestNum,
+                    })
+                    self.conn.send_data(seg, (self.host,self.destPort))
+                    print(f"[Segment SEQ={sequenceNum+1}] received. Ack sent to {self.host}:{self.destPort}")
+                else: # FIN flag
+                    file.write(rcvSeg.get_payload())
+                    seg = segment.Segment()
+                    seg.set_flag([1,0,1])
+                    seg.set_header({
+                        'seq_num': sequenceNum,
+                        'ack_num': requestNum,
+                    })
+                    self.conn.send_data(seg, (self.host,self.destPort))
+                    print(f"[Segment SEQ={sequenceNum+1}] received. Ack sent to {self.host}:{self.destPort}")
+                    break
+            else:
+                errSeg = segment.Segment()
+                errSeg.set_flag([0,0,0])
+                errSeg.set_header({
+                    'ack_num': requestNum,
+                })
+                error = True
+                self.conn.send_data(errSeg, (self.host,self.destPort))
+                print(f"[Segment SEQ={sequenceNum+1}] damaged. Ack {sequenceNum} sent to {self.host}:{self.destPort}")
         
-        if not(data.get_syn()) and not(data.get_ack()) and not(data.get_fin()):
-            header = data.get_header()
-            serverACK = header['ack_num']
-            serverSeq = header['seq_num']
-            data.set_header({
-            'seq_num': serverACK,
-            'ack_num': serverSeq+1,
-            })
-            data.set_flag([0,0,1])
-            print(f"[Segment SEQ={serverSeq}] Received, Ack sent")
-            self.conn.send_data(data, (self.host,self.destPort))
-        with open(self.outputPath, 'wb') as f:
-            f.write(data.get_payload())
+        if (error):
+            print("[!] Go-Back-N protocol success.")
+        print("[!] File transfer completed.")
+        
+        file.close()
+        self.close_connection()
+        
+        # if not(data.get_syn()) and not(data.get_ack()) and not(data.get_fin()):
+        #     header = data.get_header()
+        #     serverACK = header['ack_num']
+        #     serverSeq = header['seq_num']
+        #     data.set_header({
+        #     'seq_num': serverACK,
+        #     'ack_num': serverSeq+1,
+        #     })
+        #     data.set_flag([0,0,1])
+        #     print(f"[Segment SEQ={serverSeq}] Received, Ack sent")
+        #     self.conn.send_data(data, (self.host,self.destPort))
+        # with open(self.outputPath, 'wb') as f:
+        #     f.write(data.get_payload())
+
+    def close_connection(self):
+        # Close connection, client-side
+        print("[!] Closing connection...")
+        fw = segment.Segment()
+        fw.set_flag([1,0,1])
+        print("Sending FIN to server...")
+        
+        self.conn.send_data(fw, (self.host,self.destPort))
+        print("Waiting for server response...")
+        self.conn.listen_single_segment()
+        print("Received ACK from server")
+        self.conn.listen_single_segment()
+        print("Received FIN-ACK from server")
+
+        fw3 = segment.Segment()
+        fw3.set_flag([0,0,1])
+        print("Sending ACK to server...")
+        self.conn.send_data(fw3, (self.host,self.destPort))
+        print(f"[!] Connection closed with {self.host}:{self.destPort}")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
